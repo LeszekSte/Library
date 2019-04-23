4bf2da45185c6bd2f59baab4d704c670e3323b82
LeszekS <leszekstepien@o2.pl>
2019-04-23 14:43:23 +0200

    Version 1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,22 @@
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        final String version = "1.0";
+
+        String title = "W pustyni i w puszczy";
+        String author = "Henryk Sienkiewicz";
+        int yearOfPublishmen = 1955;
+        int numberOfPages = 256;
+        String publishingHouse = "Helios";
+        String isbnNUmber = "65464567414";
+
+        System.out.println("Tutuł - " + title);
+        System.out.println("Autor - " + author);
+        System.out.println("Rok wydania - " + yearOfPublishmen);
+        System.out.println("Ilość stron -  " + numberOfPages);
+        System.out.println("ISBN numer  - " + isbnNUmber);
+
+
+
     }
 }
