4f196459830692a06586cac38eaa1b0136068c0d
LeszekS <leszekstepien@o2.pl>
2019-04-26 14:38:07 +0200

    Version 8

--- src/library/app/Library.java
+++ src/library/app/Library.java
@@ -1,26 +1,27 @@
 package library.app;
 
+import library.io.DataReader;
 import library.model.Book;
 
 public class Library {
 
     public static void main(String[] args) {
 
-        final String appName = "Biblioteka v0.5";
-
+        final String appName = "Biblioteka v0.8";
         final int VOLUMIN = 1000;
 
         Book[] books = new Book[VOLUMIN];
-
-
         books[0] = new Book("W pustyni i w puszczy", "Heniek", 1870, 300, "Agora", "1654636");
         books[1] = new Book("W pustyni", "Wojtek", 1900, 500, "Helios", "16546654636");
         books[2] = new Book("W Ameryce", "Marek", 2055, 301, "TomCat", "5461654636");
 
+        DataReader dataReader = new DataReader();
+        books[3] = dataReader.readAndCreateBook();
+
         System.out.println(appName);
-        System.out.println(books[0].toString());
-        System.out.println(books[1].toString());
-        System.out.println(books[2].toString());
+            for (int i = 0; i < 4; i++) {
+                System.out.println(books[i].toString());
+            }
 
         System.out.printf("W bibliotece można przechowywać %d książek.", books.length);
     }
