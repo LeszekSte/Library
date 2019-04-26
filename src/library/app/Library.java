abdf828158f4155e01ba1098ed0a9cfe90a491e9
LeszekS <leszekstepien@o2.pl>
2019-04-26 13:46:14 +0200

    Version 7

--- src/library/app/Library.java
+++ src/library/app/Library.java
@@ -8,20 +8,20 @@ public class Library {
 
         final String appName = "Biblioteka v0.5";
 
-        String title = "W pustyni i w puszczy";
-        String author = "Henryk Sienkiewicz";
-        int yearOfPublishmen = 1_955;
-        int numberOfPages = 256;
-        String publishingHouse = "Helios";
-        String isbnNumber = "65464567414";
+        final int VOLUMIN = 1000;
 
-        Book book = new Book(title, author, yearOfPublishmen, numberOfPages, publishingHouse);
-        Book book1 = new Book(title, author, yearOfPublishmen, numberOfPages, publishingHouse,
-                isbnNumber);
+        Book[] books = new Book[VOLUMIN];
+
+
+        books[0] = new Book("W pustyni i w puszczy", "Heniek", 1870, 300, "Agora", "1654636");
+        books[1] = new Book("W pustyni", "Wojtek", 1900, 500, "Helios", "16546654636");
+        books[2] = new Book("W Ameryce", "Marek", 2055, 301, "TomCat", "5461654636");
 
         System.out.println(appName);
-        System.out.println(book.toString());
-        System.out.println(book1.toString());
+        System.out.println(books[0].toString());
+        System.out.println(books[1].toString());
+        System.out.println(books[2].toString());
 
+        System.out.printf("W bibliotece można przechowywać %d książek.", books.length);
     }
 }
